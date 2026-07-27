c60e8209b3bacf4430a5e636215fc4e90ce6c224
Jason Gerard DeRose <jasonderose@gmail.com>
2026-07-27 14:57:39 -0600

    greet_people on 2 sec timer

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,14 @@ fn hello_world() {
     println!("hello, world.");
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -49,13 +54,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people).add_systems(
-            Update,
-            (
-                hello_world,
-                (greet_people, update_people, greet_people).chain(),
-            ),
-        );
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
