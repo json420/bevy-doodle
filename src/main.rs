e4d8f1657c9d841f64d0b65f213bf0c5002ed124
Jason Gerard DeRose <jasonderose@gmail.com>
2026-07-27 13:37:11 -0600

    hello, world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+struct Entity(u64);
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
+fn hello_world() {
+    println!("hello, world.");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_systems(Update, hello_world).run();
 }
