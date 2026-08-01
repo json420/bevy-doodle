0af3d0cea32c77a3b6d1866bcd100f4b6e6795d3
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-01 15:54:10 -0600

    remove unused person/name/greeting stuffs

--- src/main.rs
+++ src/main.rs
@@ -5,48 +5,6 @@ use bevy::{
     window::{PresentMode, WindowPlugin},
 };
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Joan".to_string())));
-    commands.spawn((Person, Name("Tina".to_string())));
-    commands.spawn((Person, Name("Fred".to_string())));
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Tina" {
-            name.0 = "Sue".to_string();
-            break;
-        }
-    }
-}
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
-    }
-}
-
 const ORB_COLOR: Color = Color::srgb(0.8, 0.1, 0.1);
 const ORB_INITIAL_POSITION: Vec3 = Vec3::new(0.0, -50.0, 2.0);
 const ORB_INITIAL_VELOCITY: Vec2 = Vec2::new(0.5, -0.5);
@@ -177,7 +135,6 @@ fn main() {
             }),
             ..Default::default()
         }))
-        .add_plugins(HelloPlugin)
         .insert_resource(AngularVelocityZ(0.0))
         .add_systems(Startup, setup_sensei)
         .add_systems(Startup, spawn_orb)
